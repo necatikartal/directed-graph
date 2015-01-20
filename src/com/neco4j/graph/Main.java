5a2cafbf365b2b5466750861bb2491ab9842d37d
Necati Kartal <nctkrtl@hotmail.com>
2015-01-20 21:35:32 +0200

    update test data

--- src/com/neco4j/graph/Main.java
+++ src/com/neco4j/graph/Main.java
@@ -11,30 +11,25 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		
-		// Do test here.
+	// Do test here.
 		
-		// create graph
-		Graph gr = new Graph();
+	// create graph
+	Graph gr = new Graph();
 		
-		// add vertices
-		gr.addVertex("1",40);
+	// add vertices
+	gr.addVertex("1",40);
         gr.addVertex("2",70);
         gr.addVertex("3",100);
         gr.addVertex("4",10);
 		
-		// add edges
+	// add edges
         gr.addEdge("1", "2"); 
-        gr.addEdge("1", "3");      
-        gr.addEdge("1", "4");
-                
-        gr.addEdge("2", "3");     
-        gr.addEdge("2", "4");
-        
-        gr.addEdge("3", "1");
-        gr.addEdge("3", "1");
-        gr.addEdge("3", "1");
-        
-        gr.addEdge("4", "3");
+        gr.addEdge("1", "3");
+
+        gr.addEdge("3", "2");
+
+        gr.addEdge("4", "1");
+        gr.addEdge("4", "2");
         
         // print the graph
         gr.printGraph();
